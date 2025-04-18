b0d835e99fedacab433620fcdefda27a045dac9b
hyunmin <khmin052@naver.com>
2025-04-18 18:10:07 +0900

    20250418_18시00분 수정본

--- 계산기 과제/workspace/CalculatorProject/src/Project/example2/App.java
+++ 계산기 과제/workspace/CalculatorProject/src/Project/example2/App.java
@@ -8,7 +8,11 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
+        // 입력 Scanner
         Scanner scanner = new Scanner(System.in);
+
+        // 계산기 클래스 Calculator
+        Calculator calculator = new Calculator();
 
         String request = null;
 
@@ -18,21 +22,32 @@ public class App {
 //        String div = "/";
 //        String mod = "%";
         String exit = "exit";
+        String history = "이전기록";
 
         int num1;
         int num2;
-        int result;
-
+        double result;
+        
 
         // 계산기 반복문
-        for (;;) {
-            System.out.print("계산 기호 입력(ex: +, -, X, /, %) 종료하려면 exit입력: ");
+        while (true){
+            System.out.print("계산 기호(ex: +, -, X, /, %)또는 '이전기록' 입력 종료하려면 exit입력: ");
             String input = scanner.nextLine();
 
             // 탈출부분
             if (input.equals(exit)) {
                 System.out.println("계산기를 종료합니다.");
-                return;
+                break;
+            }
+
+            // 이전기록 출력
+            if (input.equals(history)) {
+                System.out.println();
+                System.out.println("==== 이전 계산 히스토리 ====");
+                for (int i = 0; i < calculator.getHistory().size(); i++) {
+                    System.out.println(calculator.getHistory().get(i));
+                }
+                continue;
             }
 
             // 입력이 비었을 경우
@@ -47,199 +62,52 @@ public class App {
             num2 =0;
             result = 0;
 
+            String messageNum1 = "첫번째 수를 입력해주세요(0또는 양의정수): ";
+            String messageNum2 = "두번째 수를 입력해주세요(0또는 양의정수): ";
+
             switch (res) {
                 case '+':
-                    System.out.print("첫번째 숫자를 입력해주세요(0, 양의정수): ");
-                    num1 = scanner.nextInt();
+                    num1 = getInput(scanner, messageNum1);
+                    num2 = getInput(scanner, messageNum2);
 
-                    if (num1 < 0) {
-                        for (;;) {
-                            num1 = 0;
-                            System.out.println("음의 정수는 입력이 불가합니다.\n 다시 입력해주세요");
-                            System.out.print("첫번째 숫자를 입력해주세요(0, 양의정수): ");
-                            num1 = scanner.nextInt();
-
-                            if (num1 >= 0){
-                                break;
-                            }
-                        }
-                    }
-                    System.out.print("두번째 숫자를 입력해주세요(0, 양의정수): ");
-                    num2 = scanner.nextInt();
-
-                    scanner.nextLine();
-                    if (num2 < 0) {
-                        for (;;) {
-                            num2 = 0;
-                            System.out.println("음의 정수는 입력이 불가합니다.\n 다시 입력해주세요");
-                            System.out.print("첫번째 숫자를 입력해주세요(0, 양의정수): ");
-                            num2 = scanner.nextInt();
-
-                            if (num2 >= 0){
-                                break;
-                            }
-                        }
-                    }
-
-
-                    result = num1 + num2;
-
-                    System.out.println(num1 + " + " + num2 + " = " + result);
-
+                    result = calculator.sum(num1, num2);
+                    System.out.println(num1 + " + " + num2 + " = " + (int) result);
                     break;
 
                 case '-':
-                    System.out.print("첫번째 숫자를 입력해주세요(0, 양의정수): ");
-                    num1 = scanner.nextInt();
-                    if (num1 < 0) {
-                        for (;;) {
-                            num1 = 0;
-                            System.out.println("음의 정수는 입력이 불가합니다.\n 다시 입력해주세요");
-                            System.out.print("첫번째 숫자를 입력해주세요(0, 양의정수): ");
-                            num1 = scanner.nextInt();
+                    num1 = getInput(scanner, messageNum1);
+                    num2 = getInput(scanner, messageNum2);
 
-                            if (num1 >= 0){
-                                break;
-                            }
-                        }
-                    }
-                    System.out.print("두번째 숫자를 입력해주세요(0, 양의정수): ");
-                    num2 = scanner.nextInt();
-
-                    scanner.nextLine();
-                    if (num2 < 0) {
-                        for (;;) {
-                            num2 = 0;
-                            System.out.println("음의 정수는 입력이 불가합니다.\n 다시 입력해주세요");
-                            System.out.print("첫번째 숫자를 입력해주세요(0, 양의정수): ");
-                            num2 = scanner.nextInt();
-
-                            if (num2 >= 0){
-                                break;
-                            }
-                        }
-                    }
-
-                    result = num1 - num2;
-
-                    System.out.println(num1 + " - " + num2 + " = " + result);
-
+                    result = calculator.sub(num1, num2);
+                    System.out.println(num1 + " + " + num2 + " = " + (int) result);
                     break;
 
                 case 'X':
-                    System.out.print("첫번째 숫자를 입력해주세요(0, 양의정수): ");
-                    num1 = scanner.nextInt();
-                    if (num1 < 0) {
-                        for (;;) {
-                            num1 = 0;
-                            System.out.println("음의 정수는 입력이 불가합니다.\n 다시 입력해주세요");
-                            System.out.print("첫번째 숫자를 입력해주세요(0, 양의정수): ");
-                            num1 = scanner.nextInt();
+                    num1 = getInput(scanner, messageNum1);
+                    num2 = getInput(scanner, messageNum2);
 
-                            if (num1 >= 0){
-                                break;
-                            }
-                        }
-                    }
-                    System.out.print("두번째 숫자를 입력해주세요(0, 양의정수): ");
-                    num2 = scanner.nextInt();
-
-                    scanner.nextLine();
-                    if (num2 < 0) {
-                        for (;;) {
-                            num2 = 0;
-                            System.out.println("음의 정수는 입력이 불가합니다.\n 다시 입력해주세요");
-                            System.out.print("첫번째 숫자를 입력해주세요(0, 양의정수): ");
-                            num2 = scanner.nextInt();
-
-                            if (num2 >= 0){
-                                break;
-                            }
-                        }
-                    }
-
-                    result = num1 * num2;
-
-                    System.out.println(num1 + " X " + num2 + " = " + result);
-
+                    result = calculator.mul(num1, num2);
+                    System.out.println(num1 + " + " + num2 + " = " + (int) result);
                     break;
 
                 case '/':
+                    String divMsg1 = "분자에 들어갈 수를 입력해주세요(0또는 양의정수): ";
+                    num1 = getInput(scanner, divMsg1);
+                    String divMsg2 = "분모에 들어갈 양수를 입력해주세요(0 불가): ";
+                    num2 = getPositiveInput(scanner, divMsg2);
 
-                    System.out.print("분자를 입력해주세요(0, 양의정수): ");
-                    num1 = scanner.nextInt();
-                    if (num1 < 0) {
-                        for (;;) {
-                            num1 = 0;
-                            System.out.println("음의 정수는 입력이 불가합니다.\n 다시 입력해주세요");
-                            System.out.print("분자를 입력해주세요(0, 양의정수): ");
-                            num1 = scanner.nextInt();
-
-                            if (num1 >= 0){
-                                break;
-                            }
-                        }
-                    }
-                    System.out.print("분모를 입력해주세요(0, 양의정수): ");
-                    num2 = scanner.nextInt();
-
-                    if (num2 <= 0) {
-                        num2 = 0;
-                        for (;;) {
-                            System.out.println("분모는 0보다 작을 수 없습니다. 다시 입력해주세요.");
-                            System.out.print("분모를 입력해주세요(0, 양의정수): ");
-                            num2 = scanner.nextInt();
-
-                            if (num2 > 0) {
-                                break;
-                            }
-
-                            scanner.nextLine();
-                        }
-                    }
-
-                    double div = (double)num1 / (double)num2;
-
-                    System.out.println(num1 + " / " + num2 + " = " + result);
-
+                    result = calculator.div(num1, num2);
+                    System.out.println(num1 + " + " + num2 + " = " + result);
                     break;
 
                 case '%':
-                    System.out.print("첫번째 숫자를 입력해주세요(0, 양의정수): ");
-                    num1 = scanner.nextInt();
-                    if (num1 < 0) {
-                        for (;;) {
-                            num1 = 0;
-                            System.out.println("음의 정수는 입력이 불가합니다.\n 다시 입력해주세요");
-                            System.out.print("첫번째 숫자를 입력해주세요(0, 양의정수): ");
-                            num1 = scanner.nextInt();
+                    String modMsg1 = "나누어질 수를 입력해주세요(0또는 양의정수): ";
+                    num1 = getInput(scanner, modMsg1);
+                    String modMsg2 = "나눌 수를 입력해주세요(0 불가): ";
+                    num2 = getPositiveInput(scanner, modMsg2);
 
-                            if (num1 >= 0){
-                                break;
-                            }
-                        }
-                    }
-                    System.out.print("두번째 숫자를 입력해주세요(0, 양의정수): ");
-                    num2 = scanner.nextInt();
-
-                    scanner.nextLine();
-                    if (num2 < 0) {
-                        for (;;) {
-                            num2 = 0;
-                            System.out.println("음의 정수는 입력이 불가합니다.\n 다시 입력해주세요");
-                            System.out.print("첫번째 숫자를 입력해주세요(0, 양의정수): ");
-                            num2 = scanner.nextInt();
-
-                            if (num2 >= 0){
-                                break;
-                            }
-                        }
-                    }
-
-                    result = num1 % num2;
-
-                    System.out.println(num1 + " % " + num2 + " = " + result);
-
+                    result = calculator.mod(num1, num2);
+                    System.out.println(num1 + " + " + num2 + " = " + (int) result);
                     break;
 
                 default:
@@ -248,6 +116,51 @@ public class App {
 
             }
             System.out.println();
+        }
+    }
+
+    // 0혹은 양의정수만 받는 함수
+    private static int getInput(Scanner scanner, String message) {
+        int inputNum;
+        while (true) {
+            System.out.print(message);
+            if (scanner.hasNextInt()){
+                inputNum = scanner.nextInt();
+                if (inputNum >= 0) {
+                    scanner.nextLine();
+                    return inputNum;
+                } else {
+
+                    System.out.println("0 또는 양수만 입력이 가능합니다. 다시 시도해주세요.");
+                    scanner.nextLine(); // 잘못된 입력 제거
+                }
+            } else {
+                System.out.println("허용된 범위내에 정수(0또는 양수)만 입력 가능합니다. 다시 시도해주세요.");
+                scanner.next(); // 잘못된 입력 소비
+            }
+        }
+    }
+
+    // 0 불가 양의정수만 받는 함수
+    // 0혹은 양의정수만 받는 함수
+    private static int getPositiveInput(Scanner scanner, String message) {
+        int inputNum;
+        while (true) {
+            System.out.print(message);
+            if (scanner.hasNextInt()){
+                inputNum = scanner.nextInt();
+
+                if (inputNum > 0) {
+                    scanner.nextLine();
+                    return inputNum;
+                } else {
+                    System.out.println("양수만 입력이 가능합니다. 다시 시도해주세요.");
+                    scanner.nextLine(); // 잘못된 입력 제거
+                }
+            } else {
+                System.out.println("허용된 범위내에 정수(0또는 양수)만 입력 가능합니다. 다시 시도해주세요.");
+                scanner.next(); // 잘못된 입력 소비
+            }
         }
     }
 }
--- 계산기 과제/workspace/CalculatorProject/src/Project/example2/Calculator.java
+++ 계산기 과제/workspace/CalculatorProject/src/Project/example2/Calculator.java
@@ -1,23 +1,29 @@
 package Project.example2;
 
+import java.util.ArrayList;
+
 public class Calculator {
 
     // 속성
-    private int num1 = 0; // 첫번째 수
-    private int num2 = 0; // 두번째 수
+    private double num1;
+    private double num2;
+    private String operator;
+    private ArrayList<String> history;
 
     // 생성자
-    public Calculator(int num1, int num2) {
-        this.num1 = num1;
-        this.num2 = num2;
+    public Calculator() {
+        history = new ArrayList<>();
     }
 
     // 게터
-    public int getNum1() {
+    public double getNum1() {
         return num1;
     }
-    public int getNum2() {
+    public double getNum2() {
         return num2;
+    }
+    public ArrayList<String> getHistory() {
+        return history;
     }
 
     // 세터
@@ -31,22 +37,38 @@ public class Calculator {
     // 기능
     // 더하기
     public int sum(int num1, int num2) {
-        return num1 + num2;
+        operator = "+";
+        int result = num1 + num2;
+        history.add(num1 + " " + operator + " " + num2 + " = " + result);
+        return result;
     }
     // 빼기
     public int sub(int num1, int num2) {
-        return num1 - num2;
+        operator = "-";
+        int result = num1 - num2;
+        history.add(num1 + " " + operator + " " + num2 + " = " + result);
+        return result;
     }
     // 곱하기
     public int mul(int num1, int num2) {
-        return num1 * num2;
+        operator = "X";
+        int result = num1 * num2;
+        history.add(num1 + " " + operator + " " + num2 + " = " + result);
+        return result;
     }
     // 나누기
-    public double div(int num1, int num2) {
-        return (double)num1 / num2;
+    public int div(int num1, int num2) {
+        operator = "/";
+        int result = num1 / num2;
+        history.add(num1 + " " + operator + " " + num2 + " = " + result);
+        return result;
     }
     // 나머지
     public int mod(int num1, int num2) {
-        return num1 % num2;
+        operator = "%";
+        int result = num1 % num2;
+        history.add(num1 + " " + operator + " " + num2 + " = " + result);
+        return result;
     }
+
 }
